2da7becc250bcba35645a9908da1275a9ad5dcc3
client_always_right <fgh757537@gmail.com>
2026-06-29 04:37:35 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/название-твоего-репозитория/', // Важно: добавь слеши в начале и конце
   server: {
     host: '127.0.0.1',
     port: 5173,
